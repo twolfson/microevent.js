5c160a7b533f72a5178671c1b24caaf5d306a208
Todd Wolfson <todd.wolfson@ensighten.com>
2012-05-11 17:04:15 -0700

    Moved to .on, .off from .bind, .unbind

--- microevent.js
+++ microevent.js
@@ -9,25 +9,25 @@
  *   - make it safer to use
 */
 
-var MicroEvent	= function(){}
-MicroEvent.prototype	= {
-	bind	: function(event, fct){
-		this._events = this._events || {};
-		this._events[event] = this._events[event]	|| [];
-		this._events[event].push(fct);
-	},
-	unbind	: function(event, fct){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		this._events[event].splice(this._events[event].indexOf(fct), 1);
-	},
-	trigger	: function(event /* , args... */){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		for(var i = 0; i < this._events[event].length; i++){
-			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1))
-		}
-	}
+function MicroEvent() {}
+MicroEvent.prototype = {
+  on: function(event, fct){
+    this._events = this._events || {};
+    this._events[event] = this._events[event] || [];
+    this._events[event].push(fct);
+  },
+  off: function(event, fct){
+    this._events = this._events || {};
+    if (event in this._events === false) { return; }
+    this._events[event].splice(this._events[event].indexOf(fct), 1);
+  },
+  trigger: function(event /* , args... */){
+    this._events = this._events || {};
+    if( event in this._events === false  )  return;
+    for(var i = 0; i < this._events[event].length; i++){
+      this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1))
+    }
+  }
 };
 
 /**
@@ -37,14 +37,14 @@ MicroEvent.prototype	= {
  *
  * @param {Object} the object which will support MicroEvent
 */
-MicroEvent.mixin	= function(destObject){
-	var props	= ['bind', 'unbind', 'trigger'];
-	for(var i = 0; i < props.length; i ++){
-		destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
-	}
+MicroEvent.mixin = function(destObject){
+  var props  = ['on', 'off', 'trigger'];
+  for(var i = 0; i < props.length; i ++){
+    destObject.prototype[props[i]] = MicroEvent.prototype[props[i]];
+  }
 }
 
 // export in common js
-if( typeof module !== "undefined" && ('exports' in module)){
-	module.exports	= MicroEvent
+if (typeof module !== "undefined" && ('exports' in module)) {
+  module.exports = MicroEvent;
 }
