3329d15cb6d7b9ab3d6431041a1b2063ebd35e1d
Todd Wolfson <todd@twolfson.com>
2012-05-12 11:10:54 -0700

    Playing around with singleton min

---
+++ microevent.singleton.js
@@ -0,0 +1,19 @@
+{
+  'channels': [], // Define the channels
+  'on': function (channelName, fn) {
+    // Fallback the channel and add the function
+    (this.channels[channelName] = this.channels[channelName] || []).push(fn);
+  },
+  'emit': function (channelName) {
+    var channel = (this.channels[channelName] || []),
+        i = channel.length;
+    while (i--) {
+      channel[i].apply([], [].slice.call(arguments));
+    }
+  }
+  // TODO: Work in unsubscribing
+  // 'off': function (channelName, fn) {
+    // // Fallback the channel and add the function
+    // (this.channels[channelName] = this.channels[channelName] || []).push(fn);
+  // }
+}
